66085bc18a05f88e23ac226551814434df290d3d
Travis Dean <travis.j.dean@gmail.com>
2015-04-06 19:14:36 -0400

    Completed parsing code.

--- hw5.py
+++ hw5.py
@@ -5,14 +5,29 @@ import numpy as np
 __author__ = 'Travis Dean'
 debug = True
 
+def solve(requests, classes):
+
+    return True
+
 def read_data(filename):
     with open(filename) as f:
-        cases = []
         registrations, courses, numclasses = f.readline().split()
+        if (registrations is 0 and courses is 0 and numclasses is 0): return
+        requests = {}
         for _ in range(int(registrations)):
             student, course = f.readline().split()
-            pass
+            if (requests.get(student)):
+                requests[student].append(course)
+            else:
+                requests[student] = [course]
+        classes = {}
+        for _ in range(int(courses)):
+            course, capacity = f.readline().split()
+            classes[course] = capacity
 
+        print("Yes") if solve(requests, classes) else print("No")
+
+        f.readline()    # Remove blank line
 
 if __name__ == "__main__":
     if not debug:
